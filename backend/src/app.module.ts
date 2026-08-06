44437bf13a10d4127fa65a9b4c85177c422245e9
dsitweed <luffy3042001@gmail.com>
2026-08-06 17:13:12 +0900

    refactor [backend]: reorganize module imports and enable guards in AppModule

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,21 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { APP_GUARD } from '@nestjs/core';
 
 import { AppController } from 'app.controller';
 import { AppService } from 'app.service';
 import { AuthModule } from 'auth/auth.module';
+import { BillsModule } from 'bills/bills.module';
+import { BuildingsModule } from 'buildings/buildings.module';
+import { ChatModule } from 'chat/chat.module';
+import { JwtAuthGuard, RolesGuard } from 'common/guards';
 import { validateEnv } from 'config';
+import { MaintenanceModule } from 'maintenance/maintenance.module';
+import { NotificationsModule } from 'notifications/notifications.module';
+import { PaymentsModule } from 'payments/payments.module';
+import { RentalsModule } from 'rentals/rentals.module';
+import { RoomsModule } from 'rooms/rooms.module';
+import { UsersModule } from 'users/users.module';
 
 import { PrismaModule } from './prisma/prisma.module';
 
@@ -16,27 +27,27 @@ import { PrismaModule } from './prisma/prisma.module';
     }),
     PrismaModule,
     AuthModule,
-    // UsersModule,
-    // BuildingsModule,
-    // RoomsModule,
-    // RentalsModule,
-    // BillsModule,
-    // PaymentsModule,
-    // MaintenanceModule,
-    // ChatModule,
-    // NotificationsModule,
+    UsersModule,
+    BuildingsModule,
+    RoomsModule,
+    RentalsModule,
+    BillsModule,
+    PaymentsModule,
+    MaintenanceModule,
+    ChatModule,
+    NotificationsModule,
   ],
   controllers: [AppController],
   providers: [
     AppService,
-    //   {
-    //     provide: APP_GUARD,
-    //     useClass: JwtAuthGuard,
-    //   },
-    //   {
-    //     provide: APP_GUARD,
-    //     useClass: RolesGuard,
-    //   },
+    {
+      provide: APP_GUARD,
+      useClass: JwtAuthGuard,
+    },
+    {
+      provide: APP_GUARD,
+      useClass: RolesGuard,
+    },
   ],
 })
 export class AppModule {}
