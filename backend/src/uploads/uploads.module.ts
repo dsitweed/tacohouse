1b5f8eb44b7779aec8f43179c743759d9f19f2f9
dsitweed <luffy3042001@gmail.com>
2026-08-19 15:51:28 +0900

    [BE] fix: dependencies of UploadsServices

--- backend/src/uploads/uploads.module.ts
+++ backend/src/uploads/uploads.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
+import { R2StorageService } from 'storage/r2-storage.service';
 
 import { UploadsController } from './uploads.controller';
 import { UploadsService } from './uploads.service';
 
 @Module({
   controllers: [UploadsController],
-  providers: [UploadsService],
+  providers: [UploadsService, R2StorageService],
 })
 export class UploadsModule {}
