1979900559454f744f3d9f3966a8f18358c7054b
dsitweed <luffy3042001@gmail.com>
2026-09-06 15:35:40 +0900

    [BE] feat: enhance logging interceptor to include success and error messages with detailed request and response data

--- backend/src/common/interceptors/logging.interceptor.ts
+++ backend/src/common/interceptors/logging.interceptor.ts
@@ -6,7 +6,7 @@ import {
   NestInterceptor,
 } from '@nestjs/common';
 import { Request } from 'express';
-import { Observable, tap } from 'rxjs';
+import { catchError, Observable, tap } from 'rxjs';
 
 @Injectable()
 export class LoggingInterceptor implements NestInterceptor {
@@ -22,12 +22,24 @@ export class LoggingInterceptor implements NestInterceptor {
       tap((data) => {
         const delay = Date.now() - start;
         this.logger.debug(
-          `${method} ${url} ${delay}ms
+          `${method} ${url} ${delay}ms [SUCCESS]
           Params: ${JSON.stringify(params)}
           Query: ${JSON.stringify(query)}
           Body: ${JSON.stringify(body)}
-          Data: ${data}`,
+          Response: ${data}`,
         );
+      }),
+      catchError((error: Error) => {
+        const delay = Date.now() - start;
+        this.logger.error(
+          `${method} ${url} ${delay}ms [ERROR]
+          Params: ${JSON.stringify(params)}
+          Query: ${JSON.stringify(query)}
+          Body: ${JSON.stringify(body)}
+          Error: ${error.message}
+          Stack: ${error.stack}`,
+        );
+        throw error;
       }),
     );
   }
